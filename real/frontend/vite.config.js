d59d1b9754166e2e60552e21bc66ce496ee14b65
simmerjit <131698642+simmerjit@users.noreply.github.com>
2025-07-13 23:56:38 +0530

    Update vite.config.js

--- real/frontend/vite.config.js
+++ real/frontend/vite.config.js
@@ -1,12 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+const backendURL = 'https://real-estate-webiste-fullstack.onrender.com';
+
 export default defineConfig({
   plugins: [react()],
   server: {
-   proxy: {
-      '/api': 'http://localhost:4000'
+    host: true, // Required on Render to expose dev server
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: backendURL,
+        changeOrigin: true,
+        secure: false,
+      }
     }
+  },
+  build: {
+    outDir: 'dist', // This is the directory Render looks for after build
   }
 })
