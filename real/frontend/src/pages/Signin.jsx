a78de5dd24450c723902e122ee212b2b6337d912
simmerjit <131698642+simmerjit@users.noreply.github.com>
2025-07-14 00:59:27 +0530

    Update Signin.jsx

--- real/frontend/src/pages/Signin.jsx
+++ real/frontend/src/pages/Signin.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import signimg from '../images/signimg.png';
-import '../styles/signup.css';
+import '../styles/Signup.css';
 import 'animate.css';
 
 const Signin = () => {
